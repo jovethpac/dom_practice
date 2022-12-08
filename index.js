4280c7eeefefb7bfa1100294dcdd4b8aded81f98
Joveth Sampson <jovethpac@yahoo.com>
2022-12-08 14:55:52 -0500

    edited html message and introduced load event and imported javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+    //any code inside of this function will execute after the window loads which is all of the code
+    //alert("I just created this popup");
+    
+})
